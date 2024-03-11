598829bb9b8402115abd7351fd42b4451caee555
Seonghyun Baek <bsh6954@gmail.com>
2024-03-11 12:51:04 +0900

    state

--- MyReact/myreact01/src/App.js
+++ MyReact/myreact01/src/App.js
@@ -2,12 +2,14 @@
 // => 컴포넌트는 MyComp from real_File_path; 
 //    내부 코드에서 MyComp 이름으로 인식
 import MyHeader from "./components/Header";
-import Body from "./components/Body";
+import Body from "./components01/Body04";
 import Footer from "./components/Footer";
+// import Recipe from "./components02/Recipe";
 
 function App() {
   // Test 객체를 정의하고 컴포넌트로 전달하여 출력하기
   // 1 Header로 전달
+  // bestDress 객체 생성
   const bestDress = {
     color: 'Red',
     style: 'long',
@@ -21,7 +23,9 @@ function App() {
   return (
     <div className="App">
       <MyHeader bestDress={bestDress} />
+      {/* 컴포넌트를 정의할 때 속성을 줄 수 있다 */}
       <Body name={name} country={'경기도 성남시'} />
+      {/* <Recipe /> */}
       <Footer />
     </div>
   );
