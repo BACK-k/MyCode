ff04ed548c393a46f04b100dd3c2d48c55e68075
Seonghyun Baek <bsh6954@gmail.com>
2024-03-12 17:54:14 +0900

    counter, UseEffect Hook, Life Cycle

---
+++ MyReact/myreact01/src/App01.js
@@ -0,0 +1,13 @@
+import Menu from "./components02/Menu";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2> 요리 백과 </h2>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
--- MyReact/myreact01/src/components02/IngredientsList.js
+++ MyReact/myreact01/src/components02/IngredientsList.js
@@ -2,7 +2,9 @@ import Ingredient from './Ingredient'
 
 export const IngredientsList = ({ list }) =>
     <ul className="ingredients">
+        {/* 리스트 형식은 인덱스를 넣어주는게 안전하다 */}
         {list.map((ingredient, i) =>
+            // 한 줄이라서 return 생략
             <Ingredient key={i} {...ingredient} />
         )}
     </ul>
